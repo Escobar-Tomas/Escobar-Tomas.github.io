016263b400a218349d730359945971b8e7139cfd
Tomas Escobar <escobartomas18@gmail.com>
2025-03-17 12:04:44 -0300

    Create ProductsList.js

---
+++ ProductsList.js
@@ -0,0 +1,355 @@
+window.Productos = 
+[
+    {
+        "id": 1,
+        "nombre": "Mouse Logitech M90 Black",
+        "precio": 4150,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_13462_Mouse_Logitech_M90_Black_12e6a9a6-med.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 2,
+        "nombre": "Mouse Logitech G502 Wireless Lightspeed Hero High Performance RGB",
+        "precio": 90990,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_16569_Mouse_Logitech_G502_Wireless_Lightspeed_Hero_High_Performance_RGB__f98d2f40-med.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 3,
+        "nombre": "Mouse ASUS ROG Strix P514 Gladius III RGB 26.000DPI",
+        "precio": 69490,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_31081_Mouse_ASUS_ROG_Strix_P514_Gladius_III_RGB_26.000DPI_21d87c00-med.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 4,
+        "nombre": "Mouse Logitech G903 Lightspeed Wireless HERO 25K",
+        "precio": 104880,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_9807_Mouse_Logitech_G903_Lightspeed_Wireless_HERO_25K_86c5e07e-med.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 5,
+        "nombre": "Mouse Logitech Wireless M170 Red ",
+        "precio": 9120,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_13470_Mouse_Logitech_Wireless_M170_Red_387135cd-grn.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 6,
+        "nombre": "Mouse Logitech Wireless M280 Blue",
+        "precio": 15500,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_13488_Mouse_Logitech_Wireless_M280_Blue_367d07e7-grn.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 7,
+        "nombre": "Mouse Logitech G203 Lighsync RGB Black",
+        "precio": 24990,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_18542_Mouse_Logitech_G203_Lightsync_RGB_Black_1b839a7f-med.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 8,
+        "nombre": "Mouse Logitech G203 Lighsync RGB White",
+        "precio": 24990,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_36943_Mouse_Logitech_G203_Lightsync_RGB_White_e2e81b78-med.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 9,
+        "nombre": "Mouse Redragon Centrophorus M601 RGB",
+        "precio": 18990,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_28612_Mouse_Redragon_Centrophorus_M601_RGB_e00743a5-med.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 10,
+        "nombre": "Mouse Cougar 700M EVO Esports RGB",
+        "precio": 57490,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_34160_Mouse_Cougar_700M_EVO_Esports_RGB_ba475566-med.jpg",
+        "categoria": "mouses",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 11,
+        "nombre": "Teclado Mecanico Redragon Kumara K552 RGB Outemu Blue ESP",
+        "precio": 41990,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_21314_Teclado_Mecanico_Redragon_Kumara_K552_RGB_Outemu_Blue_ESP_ad353174-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 12,
+        "nombre": "Teclado Mecanico Redragon Dragonborn K630 RGB 60% Outemu Brown White",
+        "precio": 39790,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_29728_Teclado_Mecanico_Redragon_Dragonborn_K630_RGB_60__Outemu_Brown_White_cc705bb6-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 13,
+        "nombre": "Teclado Mecanico Redragon K585 RGB Diti Switch Brown",
+        "precio": 39170,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_14755_Teclado_Mecanico_Redragon_K585_RGB_Diti_Switch_Brown_3c6a2bbd-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 14,
+        "nombre": "Teclado Razer Ornata V3 X Low Profile",
+        "precio": 42890,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42804_Teclado_Razer_Ornata_V3_X_Low_Profile__41127c47-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 15,
+        "nombre": "Teclado Mecanico Redragon Fizz Pro K161-RGB 60% White/Pink Wireless",
+        "precio": 46990,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_35723_Teclado_Mecanico_Redragon_Fizz_Pro_K616-RGB_60__White_Pink_Wireless_ddd16c00-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 16,
+        "nombre": "Teclado Magnetico Redragon K683 Fidd 8K 60% RGB USB-C",
+        "precio": 74120,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_43017_Teclado_Magnetico_Redragon_K683_Fidd_8K_60__RGB_USB-C_aef61d8e-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 17,
+        "nombre": "Teclado Mecanico Redragon Horus K619W White RGB Low Profile Switch Red",
+        "precio": 74400,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_37647_Teclado_Mecanico_Redragon_Horus_K619W_White_RGB_Low_Profile_Switch_Red__24c226b0-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 18,
+        "nombre": "Teclado Mecanico Redragon K648 Olaf Switch Red Lineal RGB USB-C 90%",
+        "precio": 77990,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_43025_Teclado_Mecanico_Redragon_K648_Olaf_Swtich_Red_Lineal_RGB_USB-C_90__79eea7b6-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 19,
+        "nombre": "Teclado Magnetico Redragon K580 RGB PRO RGB Switch Switch Brown ESP",
+        "precio": 84990,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_21948_Teclado_Mecanico_Redragon_VATA_K580_RGB_PRO_RGB_Switch_Brown_ESP_89981c93-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {
+        "id": 20,
+        "nombre": "Teclado Logitech MX Keys Mini Rose Wireless Retroimumidado",
+        "precio": 104990,
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_40661_Teclado_Logitech_MX_Keys_Mini_Rose_Wireless_Retroluminado_19a4d4da-med.jpg",
+        "categoria": "teclados",
+        "CantidadSolicitada": 1
+    },
+    {  
+        "id": 21,  
+        "nombre": "Auriculares Gamer Wesdar GH31 Black RGB RCA Y-Adapter",  
+        "precio": 9860,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_36415_Auriculares_Gamer_Wesdar_GH31_Black_RGB_RCA_Y-Adapter_ecfb98b9-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1
+    },   
+    {  
+        "id": 22,  
+        "nombre": "Auriculares Logitech H111 Single Jack",  
+        "precio": 17480,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_1172_Auriculares_Logitech_H111_Single_Jack_add9a544-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },  
+    {  
+        "id": 23,  
+        "nombre": "Auriculares Logitech H151 Black Jack 3.5",  
+        "precio": 21400,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_43545_Auriculares_Logitech_H151_Black_Jack_3.5_58f75276-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },  
+    {  
+        "id": 24,  
+        "nombre": "Auriculares Redragon H211 Cronus RGB White",  
+        "precio": 22990,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42509_Auriculares_Redragon_H211_Cronus_RGB_White_e98cce51-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },  
+    {  
+        "id": 25,  
+        "nombre": "Auriculares JBL Tune T310C Black",  
+        "precio": 35000,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42775_Auriculares_JBL_Tune_T310C_Black__9eee8362-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },
+    {  
+        "id": 26,  
+        "nombre": "Auriculares Cougar Dive Jack 3.5",  
+        "precio": 52990,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_41697_Auriculares_Cougar_Dive_Jack_3.5_500fac2a-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },  
+    {  
+        "id": 27,  
+        "nombre": "Auriculares Cougar Attila In Ear LightWeight",  
+        "precio": 55250,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_41971_Auriculares_Cougar_Attila_In_Ear_LightWeight_842723e1-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },  
+    {  
+        "id": 28,  
+        "nombre": "Auriculares JBL Tune 500 Black",  
+        "precio": 55990,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42779_Auriculares_JBL_Tune_500_Black_5d92d26e-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },  
+    {  
+        "id": 29,  
+        "nombre": "Auriculares JBL Endurance RUN 2 In-Ear Wireless Sweatproof Black",  
+        "precio": 61990,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42782_Auriculares_JBL_Endurance_RUN_2_In-Ear_Wireless_Sweatproof_Black_282aed0c-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },  
+    {  
+        "id": 30,  
+        "nombre": "Auriculares Redragon Cybill H512 Black 7.1 USB",  
+        "precio": 62770,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_43998_Auriculares_Redragon_Cybill_H312_Black_7.1_USB_d2a9f20f-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },  
+    {  
+        "id": 31,  
+        "nombre": "Auriculares Cougar VM410 Iron",  
+        "precio": 72990,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_41714_Auriculares_Cougar_VM410_Iron_c8a92dbe-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },    
+    {  
+        "id": 32,  
+        "nombre": "Auriculares Cougar Immersa T11 EX Havoc",  
+        "precio": 72990,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_41701_Auriculares_Cougar_Immersa_TI_EX_Havoc_e9d2754b-med.jpg",
+        "categoria": "auriculares",
+        "CantidadSolicitada": 1  
+    },
+    {  
+        "id": 33,  
+        "nombre": "Monitor Lenovo ThinkVision S221-30 21.5\" FHD IPS 75Hz Anti Glare VESA",  
+        "precio": 151900,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_43816_Monitor_Lenovo_ThinkVision_S22i-30_21.5__FHD_IPS_75Hz_Anti_Glare_VESA_a2a37396-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1  
+    },  
+    {  
+        "id": 34,  
+        "nombre": "Monitor Gamer Lenovo VY229H 22\" FHD IPS 100Hz",  
+        "precio": 164900,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42479_Monitor_Gamer_ASUS_VY229HF-J_22__FHD_IPS_100Hz_446aa7a4-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },  
+    {  
+        "id": 35,  
+        "nombre": "Monitor Samsung 24\" T350H FHD IPS",  
+        "precio": 181360,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42494_Monitor_Samsung_24__T350H_FHD_IPS_1ccbba93-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },  
+    {  
+        "id": 36,  
+        "nombre": "Monitor Gamer ASUS VA24EHE 24\" FHD IPS 100Hz",  
+        "precio": 182620,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42483_Monitor_Gamer_ASUS_VA24EHF-J_24__FHD_IPS_100Hz_22873587-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },  
+    {  
+        "id": 37,  
+        "nombre": "Monitor Philips 24\" 241V8L/77",  
+        "precio": 184620,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_43562_Monitor_Philips_24__241V8L_77_110c0deb-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },  
+    {  
+        "id": 38,  
+        "nombre": "Monitor Gamer AsRock Challenger CL27F 27\" IPS FHD 100Hz Freesync",  
+        "precio": 194900,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_41505_Monitor_Gamer_AsRock_Challenger_CL27FF_27__IPS_FHD_100Hz_Freesync_24bb13d1-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },  
+    {  
+        "id": 39,  
+        "nombre": "Monitor Gamer Samsung Curvo D360 24\" FHD VA 100Hz",  
+        "precio": 199250,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_44246_Monitor_Gamer_Samsung_Curvo_D366_24__FHD_VA_100Hz_719e6a7a-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },  
+    {  
+        "id": 40,  
+        "nombre": "Monitor LG 27\" FHD ZM5500 HDMI IPS",  
+        "precio": 215440,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_39534_Monitor_LG_27__FHD_27MS500_HDMI_IPS_d557c594-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },  
+    {  
+        "id": 41,  
+        "nombre": "Monitor Samsung 27\" T350H FHD IPS Flat 75Hz",  
+        "precio": 217000,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_44160_Monitor_Samsung_27__T350H_FHD_IPS_FLAT_75Hz_ead62411-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },
+    {  
+        "id": 42,  
+        "nombre": "Monitor Gamer Samsung 24\" G3 Odyssey Full HD 144Hz G30B",  
+        "precio": 280470,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_41544_Monitor_Gamer_Samsung_24__G3_Odyssey_Full_HD_144Hz_G30B_f939ce65-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },  
+    {  
+        "id": 43,  
+        "nombre": "Monitor Gamer Acer Nitro 27\" KG271 M3 Widescreen IPS 1080p 180Hz Speakers AMD FreeSync",  
+        "precio": 315000,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_40512_Monitor_Gamer_Acer_Nitro_27__KG271_M3_Widescreen_IPS_1080p_180Hz_Speakers_AMD_FreeSync_bb277898-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    },  
+    {  
+        "id": 44,  
+        "nombre": "Monitor Gamer ASUS VG27VQ 27\" FHD IPS 180Hz Freesync Premium G-SYNC",  
+        "precio": 351490,  
+        "imagen": "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42432_Monitor_Gamer_ASUS_VG279Q3A-J_27__FHD_IPS_180Hz_Freesync_Premium_G-SYNC_9bd61bf7-med.jpg",
+        "categoria": "monitores",
+        "CantidadSolicitada": 1    
+    }  
+]
